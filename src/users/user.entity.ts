f690af01f5b1ffaf6b2832dce9eab49b218ac2f4
Jack Vatcharat <vrattananun@palo-it.com>
2025-04-27 00:37:33 +0700

    feat: initialize NestJS application with 2FA authentication

---
+++ src/users/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column({ nullable: true, type: 'text' })
+  twoFASecret: string
+
+  @Column({ default: false, type: 'boolean' })
+  enable2FA: boolean
+}
